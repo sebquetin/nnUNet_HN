e9842be51148e0890bac093ff01d566e79091128
sebquetin <sebastienquetin@hotmail.fr>
2025-07-12 13:05:58 -0400

    lost state.py file

---
+++ nnunetv2/training/nnUNetTrainer/state.py
@@ -0,0 +1,50 @@
+import os
+
+class ExperimentState:
+
+    @classmethod
+    @property
+    def dropout_trans(cls):
+        return os.environ.get("dropout_trans", True) not in [0, "0", "f", "F", "false", "False"]
+    
+    @classmethod
+    @property
+    def ct_only(cls):
+        return os.environ.get("ct_only", False) not in [0, "0", "f", "F", "false", "False"]
+    
+    @classmethod
+    @property
+    def mri_only(cls):
+        return os.environ.get("mri_only", False) not in [0, "0", "f", "F", "false", "False"]
+    
+    @classmethod
+    @property
+    def no_mirror_neither_leftright(cls):
+        return os.environ.get("no_mirror_neither_leftright", False) not in [0, "0", "f", "F", "false", "False"]
+    
+    @classmethod
+    @property
+    def reg_rigid_only(cls):
+        return os.environ.get("reg_rigid_only", True) not in [0, "0", "f", "F", "false", "False"]
+    
+    @classmethod
+    @property
+    def mem_optimized(cls):
+        return os.environ.get("mem_optimized", False) not in [0, "0", "f", "F", "false", "False"]
+
+    @classmethod
+    @property
+    def epochs(cls):
+        return int(os.environ.get("epochs", 1000))
+    
+    @classmethod
+    @property
+    def nnunet_std(cls):
+        return os.environ.get("nnunet_std", False) not in [0, "0", "f", "F", "false", "False"]
+
+
+    
+if __name__ == "__main__":
+    print(ExperimentState.epochs)
+    print(ExperimentState.mem_optimized)
+    print(ExperimentState.__dict__)
